8f34de9a035154b982c300d8b672d3c7de41daa6
jackhove <jackhove@umich.edu>
2026-04-11 17:26:34 -0500

    Added logging to ML model, added tensor structure diagram

--- models/ml_model.py
+++ models/ml_model.py
@@ -1,20 +1,28 @@
+from __future__ import annotations
+
+import logging
+
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 
 from dataset.get_dataset import Dataset
 
-dataset = Dataset()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
+dataset = Dataset().build()
 
 X_train, X_test, y_train, y_test = train_test_split(
-    dataset.X, dataset.y, test_size=0.2
+    dataset.X, dataset.y, test_size=0.2, random_state=42
 )
 
 model = GradientBoostingRegressor(
     n_estimators=500,
     max_depth=6,
     learning_rate=0.05,
-    subsample=0.8
+    subsample=0.8,
 )
 model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
+
+r2 = model.score(X_test, y_test)
+logger.info("R² on test set: %.4f", r2)
